5497187f38bf95af36e232ce7f1837413e46ca07
eraneeshansari <49119549+eraneeshansari@users.noreply.github.com>
2023-05-07 21:35:00 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,33 +1,24 @@
-#!/bin/bash
+sudo apt-get install haproxy -y
 
-# Load kernel modules for containerd
-sudo printf "overlay \n br_netfilter" > /etc/modules-load.d/containerd.conf
-sudo modprobe overlay
-sudo modprobe br_netfilter
 
-# Configure network settings for Kubernetes
-sudo tee /etc/sysctl.d/kubernetes.conf <<EOF
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-net.ipv4.ip_forward = 1
+cat <<EOF >> /etc/haproxy/haproxy.cfg
+frontend fe-apiserver
+   bind 0.0.0.0:6443
+   mode tcp
+   option tcplog
+   default_backend be-apiserver
+backend be-apiserver
+   mode tcp
+   option tcplog
+   option tcp-check
+   balance roundrobin
+   default-server inter 10s downinter 5s rise 2 fall 2 slowstart 60s maxconn 250 maxqueue 256 weight 100
+
+       server master1 $master1:6443 check
+       server master2 $master2:6443 check
 EOF
-sudo sysctl --system
 
-# Install Docker and containerd
-sudo apt-get update
-sudo apt-get install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
-sudo add-apt-repository --yes "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y containerd.io
-containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
-sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
-sudo systemctl restart containerd
-sudo systemctl enable containerd
+#restart and verify haproxy
 
-# Install Kubernetes v1.25.0-00
-sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/kubernetes.gpg
-sudo echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubelet=1.25.0-00 kubeadm=1.25.0-00 kubectl=1.25.0-00
-sudo apt-mark hold kubelet kubeadm kubectl
+systemctl restart haproxy
+systemctl status haproxy
